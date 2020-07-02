876e706c1a4c04f677acf46246cc8302638127a3
Evie Brown <evie@eviebrown.com>
2020-07-02 16:00:42 -0700

    test in main relocated to TenMil1

--- source/EngineTests.cpp
+++ source/EngineTests.cpp
@@ -13,7 +13,38 @@
 
 ******************************************************************************/
 
+#include <iostream>
+#include <chrono>
+#include <iomanip>
 
+#include "GOManager.h"
+#include "GameObject.h"
 
 #include "EngineTests.h"
 
+
+
+
+
+void TenMil1(int args) {
+	GOManager manager1, manager2;
+
+	std::chrono::steady_clock clock;
+	std::chrono::time_point start = clock.now();
+
+	for (int i = 0; i < 10000000; i++) {
+		GameObject& go = manager1.AddGO(GOManager::Cat::Players);
+		go.AddTransform();
+	}
+
+	std::chrono::time_point end = clock.now();
+	std::cout
+		<< std::chrono::duration_cast<std::chrono::microseconds>((end - start)).count()
+		<< std::endl;
+
+
+	auto something = manager1.clast(GOManager::Cat::Players);
+
+	manager1.AddGO(GOManager::Cat::Hazards);
+	manager1.AddGO(GOManager::Cat::Players);
+}
